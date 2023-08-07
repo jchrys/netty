8ebfdd58be5501319731a301923681f8f97b410a
jchrys <jchrys@me.com>
2023-08-07 21:41:20 +0900

    simplifiedBench

--- microbench/src/main/java/io/netty/microbench/buffer/ByteBufIndexOfBenchmark.java
+++ microbench/src/main/java/io/netty/microbench/buffer/ByteBufIndexOfBenchmark.java
@@ -60,12 +60,12 @@ public class ByteBufIndexOfBenchmark extends AbstractMicrobenchmark {
     @Param({ "0" })
     private byte needleByte;
 
-    @Param({ "true", "false" })
+    @Param({ "false" })
     private boolean direct;
-    @Param({ "false", "true" })
+    @Param({ "false" })
     private boolean noUnsafe;
 
-    @Param({ "false", "true" })
+    @Param({ "true" })
     private boolean pooled;
 
     @Setup(Level.Trial)
