7ffccdf0e639f0a600038b8c125c20cedb74162b
jchrys <jchrys@me.com>
2023-08-13 20:12:44 +0900

    asd

--- common/src/main/java/io/netty/util/AsciiStringUtil.java
+++ common/src/main/java/io/netty/util/AsciiStringUtil.java
@@ -88,7 +88,49 @@ public final class AsciiStringUtil {
             fromIndex += Long.BYTES;
         }
         final int byteCount = length & 7;
-        return unrolledFirstIndexOf(bytes, fromIndex, byteCount, value, (int) pattern);
+        if (byteCount == 0) {
+            return -1;
+        }
+        if (PlatformDependent.getByte(bytes, fromIndex) == value) {
+            return fromIndex;
+        }
+        if (byteCount == 1) {
+            return -1;
+        }
+        if (PlatformDependent.getByte(bytes, fromIndex + 1) == value) {
+            return fromIndex + 1;
+        }
+        if (byteCount == 2) {
+            return -1;
+        }
+        if (PlatformDependent.getByte(bytes, fromIndex + 2) == value) {
+            return fromIndex + 2;
+        }
+        if (byteCount == 3) {
+            return -1;
+        }
+        if (PlatformDependent.getByte(bytes, fromIndex + 3) == value) {
+            return fromIndex + 3;
+        }
+        if (byteCount == 4) {
+            return -1;
+        }
+        if (PlatformDependent.getByte(bytes, fromIndex + 4) == value) {
+            return fromIndex + 4;
+        }
+        if (byteCount == 5) {
+            return -1;
+        }
+        if (PlatformDependent.getByte(bytes, fromIndex + 5) == value) {
+            return fromIndex + 5;
+        }
+        if (byteCount == 6) {
+            return -1;
+        }
+        if (PlatformDependent.getByte(bytes, fromIndex + 6) == value) {
+            return fromIndex + 6;
+        }
+        return -1;
     }
 
     private static int unrolledFirstIndexOf(byte[] bytes, int fromIndex, int length, byte value, int pattern) {
