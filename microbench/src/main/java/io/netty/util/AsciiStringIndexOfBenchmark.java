cb7649911442767dc3288e08ad231416090bdbdf
jchrys <jchrys@me.com>
2023-09-08 05:31:45 +0900

    size 1 to 128 differ by inputSize test

--- microbench/src/main/java/io/netty/util/AsciiStringIndexOfBenchmark.java
+++ microbench/src/main/java/io/netty/util/AsciiStringIndexOfBenchmark.java
@@ -41,14 +41,27 @@ import java.util.concurrent.TimeUnit;
 public class AsciiStringIndexOfBenchmark extends AbstractMicrobenchmark {
 
     public static Object blackhole;
+    // from 1 to 128
     @Param({
-            "8", "15", "24", "29", "32", "47", "64"
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+            "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+            "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+            "31", "32", "33", "34", "35", "36", "37", "38", "39", "40",
+            "41", "42", "43", "44", "45", "46", "47", "48", "49", "50",
+            "51", "52", "53", "54", "55", "56", "57", "58", "59", "60",
+            "61", "62", "63", "64", "65", "66", "67", "68", "69", "70",
+            "71", "72", "73", "74", "75", "76", "77", "78", "79", "80",
+            "81", "82", "83", "84", "85", "86", "87", "88", "89", "90",
+            "91", "92", "93", "94", "95", "96", "97", "98", "99", "100",
+            "101", "102", "103", "104", "105", "106", "107", "108", "109", "110",
+            "111", "112", "113", "114", "115", "116", "117", "118", "119", "120",
+            "121", "122", "123", "124", "125", "126", "127", "128"
     })
     int size;
-    @Param({ "11" })
+    @Param({ "4", "11", "14", "17" })
     int logPermutations;
 
-    @Param({ "0" })
+    @Param({ "1" })
     int seed;
 
     int permutations;
@@ -60,9 +73,6 @@ public class AsciiStringIndexOfBenchmark extends AbstractMicrobenchmark {
 
     @Param({ "false" })
     private boolean noUnsafe;
-
-    @Param({ "1024" })
-    private int needlePosition;
 
     @Setup(Level.Trial)
     @SuppressJava6Requirement(reason = "using SplittableRandom to reliably produce data")
@@ -86,7 +96,7 @@ public class AsciiStringIndexOfBenchmark extends AbstractMicrobenchmark {
                 }
                 byteArray[j] = (byte) value;
             }
-            final int foundIndex = random.nextInt(Math.max(0, size - random.nextInt(0, 5) << 3), size);
+            final int foundIndex = random.nextInt(Math.max(0, size - 8), size);
             byteArray[foundIndex] = needleByte;
             data[i] = new AsciiString(byteArray);
             blackhole = data[i].toString(); // cache
