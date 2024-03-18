0057bc82a526382f9409f3ee5659d9a2b2acadab
jchrys <jchrys@me.com>
2024-03-18 23:02:04 +0900

    unrolled first

--- microbench/src/main/java/io/netty/microbenchmark/common/AsciiStringBenchmark.java
+++ microbench/src/main/java/io/netty/microbenchmark/common/AsciiStringBenchmark.java
@@ -17,12 +17,9 @@ package io.netty.microbenchmark.common;
 
 import io.netty.microbench.util.AbstractMicrobenchmark;
 import io.netty.util.AsciiString;
-import io.netty.util.CharsetUtil;
-import io.netty.util.internal.PlatformDependent;
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.Level;
 import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.Threads;
 import org.openjdk.jmh.annotations.Warmup;
@@ -37,8 +34,6 @@ import static java.util.concurrent.TimeUnit.MILLISECONDS;
 
 public class AsciiStringBenchmark extends AbstractMicrobenchmark {
 
-    @Param({ "3", "5", "7", "8", "10", "20", "50", "100", "1000" })
-    public int size;
 
     private AsciiString asciiString;
     private String string;
@@ -48,10 +43,6 @@ public class AsciiStringBenchmark extends AbstractMicrobenchmark {
 
     @Setup(Level.Trial)
     public void setup() {
-        byte[] bytes = new byte[size];
-        random.nextBytes(bytes);
-        asciiString = new AsciiString(bytes, false);
-        string = new String(bytes, CharsetUtil.US_ASCII);
         connection = AsciiString.cached("connection");
         Connection = AsciiString.cached("Connection");
     }
@@ -62,35 +53,8 @@ public class AsciiStringBenchmark extends AbstractMicrobenchmark {
     }
 
     @Benchmark
-    public int hashCodeBenchBytesOld() {
-        int h = 0;
-        final int end = asciiString.arrayOffset() + asciiString.length();
-        for (int i = asciiString.arrayOffset(); i < end; ++i) {
-            // masking with 0x1F reduces the number of overall bits that impact the hash code but makes the hash
-            // code the same regardless of character case (upper case or lower case hash is the same).
-            h = h * 31 + (asciiString.array()[i] & 0x1F);
-        }
-        return h;
+    public boolean equalsIgnoreCaseOld() {
+        return Connection.contentEqualsIgnoreCaseOld(connection);
     }
 
-    @Benchmark
-    public int hashCodeBenchBytesNew() {
-        return PlatformDependent.hashCodeAscii(asciiString.array(), asciiString.arrayOffset(), asciiString.length());
-    }
-
-    @Benchmark
-    public int hashCodeBenchCharSequenceOld() {
-        int h = 0;
-        for (int i = 0; i < string.length(); ++i) {
-            // masking with 0x1F reduces the number of overall bits that impact the hash code but makes the hash
-            // code the same regardless of character case (upper case or lower case hash is the same).
-            h = h * 31 + (string.charAt(i) & 0x1F);
-        }
-        return h;
-    }
-
-    @Benchmark
-    public int hashCodeBenchCharSequenceNew() {
-        return PlatformDependent.hashCodeAscii(string);
-    }
 }
